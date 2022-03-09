d545140ed1554241d156372922a7fb357934f936
rternovyi <rternovyi@itsmartflex.com>
2022-03-09 12:53:12 +0200

    Update LepContext configuration

--- src/main/java/com/icthh/xm/tmf/ms/interaction/lep/LepContext.java
+++ src/main/java/com/icthh/xm/tmf/ms/interaction/lep/LepContext.java
@@ -3,8 +3,11 @@ package com.icthh.xm.tmf.ms.interaction.lep;
 import com.icthh.xm.commons.config.client.service.TenantConfigService;
 import com.icthh.xm.commons.lep.BaseProceedingLep;
 import com.icthh.xm.commons.lep.spring.LepThreadHelper;
+import com.icthh.xm.commons.permission.service.PermissionCheckService;
 import com.icthh.xm.commons.security.XmAuthenticationContext;
 import com.icthh.xm.commons.tenant.TenantContext;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.web.client.RestTemplate;
@@ -21,14 +24,17 @@ public class LepContext {
 
     public LepServices services;
     public LepTemplates templates;
+    public MeterRegistry meterRegistry;
 
     public static class LepServices {
         TenantConfigService tenantConfigService;
         UserDetailsService userDetailsService;
         ClientDetailsService clientDetailsService;
+        PermissionCheckService permissionService;
     }
 
     public static class LepTemplates {
         public RestTemplate rest;
+        public JdbcTemplate jdbc;
     }
 }
